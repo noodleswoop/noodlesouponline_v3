1d49b7fc0b69ecd7ec9a53235fc417682142d6d8
Rachel Raabe <rachelraabe@yahoo.com>
2025-12-21 14:09:35 -0600

    Quartz sync: Dec 21, 2025, 2:09 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Rachel Beaulac",
+    pageTitle: "Noodle Soup",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "noodlesoup.online",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
